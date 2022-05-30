0677e021f8fdc4034110725ef9659b68d452cefe
“Satyam@141026” <satyam@141026@gmail.com>
2022-05-30 19:07:04 +0530

    dfsdf

---
+++ src/File1.js
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const config2 = require("./config2.json");
+export const File1 = () => {
+const [data1,setData]=useState("")
+const [pro,setPro]=useState({
+percent:0,
+load:false
+
+
+})
+const [jwt,setJwt]=useState({
+token:''
+})
+useEffect(()=>{
+  
+  let user = JSON.parse(localStorage.getItem('user'))
+  try {
+    setJwt({
+      ...jwt,
+      token:user.jwt
+
+})
+  } catch (error) {
+    console.log(error);
+  }
+},[])
+const handleChange=(e)=>{
+  console.log("Changed", e[0]);
+
+setData(e[0])
+}
+
+let  uploadImage = async(e) => {
+  setPro({
+    percent:0,
+    load:true
+  })
+  e.preventDefault();
+
+  try {
+    let data = new FormData();
+  data.append("files", data1);
+let upload_response = await axios({ method: "POST",
+      url: 'http://localhost:1337/api/upload', headers:{
+        'content-type':'application.json',
+        "Authorization":`Bearer ${jwt.token}`
+      }
+      ,data,
+      onUploadProgress:(progress)=>{
+        console.log(progress);
+        setPro( { 
+          load:true, 
+          percent: Math.round(progress.loaded / progress.total * 100)}
+        )
+      }
+    })
+    setPro({
+      load:false
+    })
+    toast("file Uploaded Successfully")
+    console.log(upload_response)
+  } catch (error) {
+    console.log(error);
+  }
+ 
+};
+/*
+let upload_response =  fetch({ method: "POST",
+      url: '/api/upload',
+      data
+    })
+
+
+*/
+  return (
+<> <form
+    className="mt-5"
+    onSubmit={(e) => {
+      uploadImage(e);}}
+  >
+    <div className="mb-3">
+      <label htmlFor="file" className="form-label">
+        Upload File
+      </label>
+      <input
+        onChange={(e) => {
+          handleChange(e.target.files);
+        }}
+        type="file"
+        accept="image/*"
+        name="files"
+        className="form-control"
+        id="file"
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+  <ToastContainer/>
+  { pro.load &&
+  <div className="progress mt-5">
+  <div className="progress-bar" role="progressbar" style={{width: pro.percent+'%'}} aria-valuenow={pro.percent} aria-valuemin={0} aria-valuemax={100}>{pro.percent}</div>
+</div>
+}
+  </>
+   
+  )
+}
+
+
---
+++ src/Pages/Login.js
@@ -0,0 +1,127 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { File1 } from '../File1'
+
+export const Login = () => {
+    const [data2,setData2]=useState({
+
+'identifier':'jaya@gmail.com'
+,'password':'Jaya@123'
+        
+    })
+    const [user,setUser]=useState({
+        is_loggedin:false,
+        user:null
+    })
+useEffect(()=>{
+try {
+ let user=   JSON.parse(localStorage.getItem("user")) 
+ if(user){
+//loggedin
+setUser({
+    ...user,
+    is_loggedin:true
+})
+ }
+ else{
+//not logged
+setUser({
+    ...user,
+    is_loggedin:false
+})
+ }
+} catch (error) {
+    
+}
+},[])
+const log1=async (e)=>{
+
+e.preventDefault()
+try {
+    const {data}=await axios.post('http://localhost:1337/api/auth/local',{
+identifier:data2.identifier,
+password:data2.password
+
+
+
+})
+console.log(data);
+setUser(
+    {
+...user,
+        is_loggedin:true
+    }
+)
+localStorage.setItem("user",JSON.stringify( data))
+} catch (error) {
+    console.log(error);
+}
+
+}
+const handleChange=(e)=>{
+
+if(e.target.classList.contains('a_username')){
+
+setData2({
+    ...data2,
+    identifier:e.target.value
+})
+
+}
+if(e.target.classList.contains('a_password')){
+    setData2({
+        ...data2,
+        password:e.target.value
+
+    })
+
+}
+
+}
+const logOut1=(e)=>{
+localStorage.removeItem('user')//remove perticular key
+//localStorage.clear()///remove all the key
+  window.location.replace('/')
+  }
+  
+  return (
+<div className="container">
+
+  <div className="row">
+    <div className="col">
+      {user.is_loggedin ||
+      <>
+         <h1>Login Form</h1>
+    <form onSubmit={(e)=>{log1(e)}}>  
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email"  onChange={(e)=>handleChange(e)} name="identifier" className="form-control a_username" id="exampleInputEmail1"  aria-describedby="emailHelp" />
+    <div id="emailHelp" className="form-text ">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1"   className="form-label">Password</label>
+    <input type="password"  name="password" onChange={(e)=>handleChange(e)} className="form-control a_password" id="exampleInputPassword1" />
+  </div>                    
+ 
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+      </> 
+      }
+     
+    </div>
+    
+  </div>
+  { user.is_loggedin && 
+  <>
+  <button onClick={(e)=>{logOut1(e)}} className='btn btn-syccess text-center' >Logout</button>
+<File1/>
+
+</>
+
+  }
+
+</div>
+
+
+  )
+}
